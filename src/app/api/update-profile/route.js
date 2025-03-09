e89b5a26be6c445f16c84cb026fad023e47ed197
prathamgarg03 <gargpratham.312003@gmail.com>
2025-03-08 17:51:28 -0800

    Fix ui bugs

--- src/app/api/update-profile/route.js
+++ src/app/api/update-profile/route.js
@@ -1,21 +1,13 @@
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-import { clerkClient } from "@clerk/nextjs";
 
 export async function POST(req) {
     try {
         const { userId } = auth();
         if (!userId) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
 
-        const { username, phoneNumber } = await req.json();
 
-        await clerkClient.users.updateUser(userId, {
-            publicMetadata: {
-                profileCompleted: true,
-                username,
-                phoneNumber,
-            },
-        });
+
 
         return NextResponse.json({ success: true });
     } catch (error) {
